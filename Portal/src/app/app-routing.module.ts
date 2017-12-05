746d7fb3015731e9abf9cec7c1aaf79b3d32a0c4
rabakozi <rabakozi@gmail.com>
2017-12-05 21:43:41 +0100

    fixing cript error and maybe missing font too

--- Portal/src/app/app-routing.module.ts
+++ Portal/src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { UserProfileComponent } from "./home/user-profile.component";
 
 @NgModule({
   imports: [RouterModule.forRoot([
-    { pathMatch: 'full', path: 'home/home', component: HomeComponent },
-    { pathMatch: 'full', path: 'home/register', component: RegisterComponent },
-    { pathMatch: 'full', path: 'home/login', component: LoginComponent },
-    { pathMatch: 'full', path: 'home/user-profile', component: UserProfileComponent },
-    { pathMatch: 'full', path: 'notes', loadChildren: './notes/note.module#NoteModule' },
-    { pathMatch: 'full', path: '*', component: HomeComponent },])],
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'home/home', component: HomeComponent, pathMatch: 'full' },
+    { path: 'home/register', component: RegisterComponent, pathMatch: 'full' },
+    { path: 'home/login', component: LoginComponent, pathMatch: 'full' },
+    { path: 'home/user-profile', component: UserProfileComponent, pathMatch: 'full' },
+    { path: 'notes', loadChildren: './notes/note.module#NoteModule', pathMatch: 'full' },
+    { path: '*', component: HomeComponent, pathMatch: 'full' },])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
